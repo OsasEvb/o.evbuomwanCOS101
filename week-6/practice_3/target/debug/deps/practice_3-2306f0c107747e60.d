db911b074c1b087bf0c536767bf50306fda2aab7
unknown <osasogie.evbuomwan@pau.edu.ng>
2025-11-03 21:26:59 +0100

    (Week 6 practiice_3)

---
+++ week-6/practice_3/target/debug/deps/practice_3-2306f0c107747e60.d
@@ -0,0 +1,5 @@
+C:\Users\Osaso\o.evbuomwanCOS101\week-6\practice_3\target\debug\deps\practice_3-2306f0c107747e60.d: src\main.rs
+
+C:\Users\Osaso\o.evbuomwanCOS101\week-6\practice_3\target\debug\deps\libpractice_3-2306f0c107747e60.rmeta: src\main.rs
+
+src\main.rs:
