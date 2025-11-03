7cdb29fbadb586d3f4fe65470545ef1654f3a479
unknown <osasogie.evbuomwan@pau.edu.ng>
2025-11-03 22:26:15 +0100

    (Week 6 practice_9)

---
+++ week-6/practice_9/target/debug/deps/practice_9-686542d0026bdc18.d
@@ -0,0 +1,5 @@
+C:\Users\Osaso\o.evbuomwanCOS101\week-6\practice_9\target\debug\deps\practice_9-686542d0026bdc18.d: src\main.rs
+
+C:\Users\Osaso\o.evbuomwanCOS101\week-6\practice_9\target\debug\deps\libpractice_9-686542d0026bdc18.rmeta: src\main.rs
+
+src\main.rs:
---
+++ week-6/practice_9/target/debug/practice_9.d
@@ -0,0 +1,1 @@
+C:\Users\Osaso\o.evbuomwanCOS101\week-6\practice_9\target\debug\practice_9.exe: C:\Users\Osaso\o.evbuomwanCOS101\week-6\practice_9\src\main.rs
