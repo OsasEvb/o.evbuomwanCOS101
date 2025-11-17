92b424ab96ef77665920ca2377a37db10fbe6c34
unknown <osasogie.evbuomwan@pau.edu.ng>
2025-11-17 18:12:36 +0100

    (Week 8 practice 9)

---
+++ week-8/practice_9/target/debug/deps/practice_9.d
@@ -0,0 +1,5 @@
+C:\Users\Osaso\o.evbuomwanCOS101\week-8\practice_9\target\debug\deps\practice_9.d: src\main.rs
+
+C:\Users\Osaso\o.evbuomwanCOS101\week-8\practice_9\target\debug\deps\practice_9.exe: src\main.rs
+
+src\main.rs:
---
+++ week-8/practice_9/target/debug/practice_9.d
@@ -0,0 +1,1 @@
+C:\Users\Osaso\o.evbuomwanCOS101\week-8\practice_9\target\debug\practice_9.exe: C:\Users\Osaso\o.evbuomwanCOS101\week-8\practice_9\src\main.rs
