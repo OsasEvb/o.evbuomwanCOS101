06757576f2426291f45bf3be0b7d596e8e277999
unknown <osasogie.evbuomwan@pau.edu.ng>
2025-11-17 11:36:57 +0100

    (Week 8 practice 2)

---
+++ week-8/practice_2/target/debug/deps/practice_2-8310848e0a36f6c9.d
@@ -0,0 +1,5 @@
+C:\Users\Osaso\o.evbuomwanCOS101\week-8\practice_2\target\debug\deps\practice_2-8310848e0a36f6c9.d: src\main.rs
+
+C:\Users\Osaso\o.evbuomwanCOS101\week-8\practice_2\target\debug\deps\libpractice_2-8310848e0a36f6c9.rmeta: src\main.rs
+
+src\main.rs:
---
+++ week-8/practice_2/target/debug/practice_2.d
@@ -0,0 +1,1 @@
+C:\Users\Osaso\o.evbuomwanCOS101\week-8\practice_2\target\debug\practice_2.exe: C:\Users\Osaso\o.evbuomwanCOS101\week-8\practice_2\src\main.rs
