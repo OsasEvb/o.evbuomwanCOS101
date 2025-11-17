922579cdfcfbf673c2fc65c7fa741021b61bfbe7
unknown <osasogie.evbuomwan@pau.edu.ng>
2025-11-17 17:57:30 +0100

    (Week 8 practice 8)

---
+++ week-8/practice_7/target/debug/deps/practice_7-0d9333737dd2d991.d
@@ -0,0 +1,5 @@
+C:\Users\Osaso\o.evbuomwanCOS101\week-8\practice_7\target\debug\deps\practice_7-0d9333737dd2d991.d: src\main.rs
+
+C:\Users\Osaso\o.evbuomwanCOS101\week-8\practice_7\target\debug\deps\libpractice_7-0d9333737dd2d991.rmeta: src\main.rs
+
+src\main.rs:
