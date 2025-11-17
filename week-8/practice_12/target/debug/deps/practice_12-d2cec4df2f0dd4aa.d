3398283d8d745cf72ca1bb129769123c53c1c9ac
unknown <osasogie.evbuomwan@pau.edu.ng>
2025-11-17 18:27:56 +0100

    (Week 8 practice_12)

---
+++ week-8/practice_12/target/debug/deps/practice_12-d2cec4df2f0dd4aa.d
@@ -0,0 +1,5 @@
+C:\Users\Osaso\o.evbuomwanCOS101\week-8\practice_12\target\debug\deps\practice_12-d2cec4df2f0dd4aa.d: src\main.rs
+
+C:\Users\Osaso\o.evbuomwanCOS101\week-8\practice_12\target\debug\deps\libpractice_12-d2cec4df2f0dd4aa.rmeta: src\main.rs
+
+src\main.rs:
---
+++ week-8/practice_12/target/debug/practice_12.d
@@ -0,0 +1,1 @@
+C:\Users\Osaso\o.evbuomwanCOS101\week-8\practice_12\target\debug\practice_12.exe: C:\Users\Osaso\o.evbuomwanCOS101\week-8\practice_12\src\main.rs
