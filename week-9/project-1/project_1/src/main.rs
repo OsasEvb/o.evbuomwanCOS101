c58232ae0a39eb8721924ca4001961082e3f8a45
unknown <osasogie.evbuomwan@pau.edu.ng>
2025-11-24 11:10:52 +0100

    (Week 9 project 1)

---
+++ week-9/project-1/project_1/src/main.rs
@@ -0,0 +1,52 @@
+use std::fs::File;
+use std::io::{self, Write};
+
+fn main() -> io::Result<()> {
+    // Drinks categorized by type
+    let lager = vec![
+        "33 Export",
+        "Desperados",
+        "Goldberg",
+        "Gulder",
+        "Heineken",
+        "Star",
+    ];
+
+    let stout = vec![
+        "Legend",
+        "Turbo King",
+        "Williams",
+    ];
+
+    let non_alcoholic = vec![
+        "Maltina",
+        "Amstel Malta",
+        "Malta Gold",
+        "Fayrouz",
+    ];
+
+    // Create the output file
+    let mut file = File::create("drinks.txt")?;
+
+    // Write data into the file
+    writeln!(file, "Nigerian Breweries Drink Categories\n")?;
+
+    writeln!(file, "Lager:")?;
+    for item in lager {
+        writeln!(file, "- {}", item)?;
+    }
+
+    writeln!(file, "\nStout:")?;
+    for item in stout {
+        writeln!(file, "- {}", item)?;
+    }
+
+    writeln!(file, "\nNon-Alcoholic:")?;
+    for item in non_alcoholic {
+        writeln!(file, "- {}", item)?;
+    }
+
+    println!("File 'drinks.txt' created successfully!");
+
+    Ok(())
+}
---
+++ week-9/project-1/project_1/target/debug/project_1.d
@@ -0,0 +1,1 @@
+C:\Users\Osaso\o.evbuomwanCOS101\week-9\project-1\project_1\target\debug\project_1.exe: C:\Users\Osaso\o.evbuomwanCOS101\week-9\project-1\project_1\src\main.rs
